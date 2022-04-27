97352d479265cae9106ef00915b7b99c86f6f173
whdiaztorres <whdiaztorres@gmail.com>
2022-04-27 10:57:25 -0300

    serviços de camadas e componentes de registros

--- src/main/java/com/projeto/curosFinalizado/recursos/UsuarioRecursos.java
+++ src/main/java/com/projeto/curosFinalizado/recursos/UsuarioRecursos.java
@@ -1,20 +1,34 @@
 package com.projeto.curosFinalizado.recursos;
 
 import com.projeto.curosFinalizado.entidad.Usuario;
+import com.projeto.curosFinalizado.serviços.ServicosDeUsuarios;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioRecursos {
 
+    @Autowired
+    private ServicosDeUsuarios servicos;
+
     @GetMapping
-    public ResponseEntity<Usuario> findAll() {
-        Usuario u = new Usuario(1L, "chemberlay",
-                "chemberlay@gmail.com", "1345643090", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<Usuario>> findAll() {
+        List<Usuario> list = servicos.findAll();
+        return ResponseEntity.ok().body(list);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public  ResponseEntity<Usuario> findById(@PathVariable Long id){
+        Usuario obj = servicos.findbyID(id);
+        return ResponseEntity.ok().body(obj);
 
     }
 }
---
+++ src/main/java/com/projeto/curosFinalizado/serviços/ServicosDeUsuarios.java
@@ -0,0 +1,26 @@
+package com.projeto.curosFinalizado.serviços;
+
+import com.projeto.curosFinalizado.entidad.Usuario;
+import com.projeto.curosFinalizado.repositorios.RepositorioDeUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServicosDeUsuarios {
+
+    @Autowired
+    private RepositorioDeUsuario repositorio;
+    public List<Usuario>  findAll(){
+        return repositorio.findAll();
+    }
+
+    public Usuario findbyID(long  id){
+       Optional<Usuario> obj = repositorio.findById(id);
+       return obj.get();
+    }
+
+}
